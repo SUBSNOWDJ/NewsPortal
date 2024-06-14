2528adc6cd9f372a10d492b8f0cd2dfa1a5d3717
SUBSNOWDJ <161745304+SUBSNOWDJ@users.noreply.github.com>
2024-06-14 22:50:27 +0300

    Delete NewsPaper directory

--- NewsPaper/news/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from django.contrib.auth.models import Group
-
-from .models import Post
-
-class PostForm(forms.ModelForm):
-    class Meta:
-        model = Post
-        fields = ['author', 'type', 'title', 'text']
